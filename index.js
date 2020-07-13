68631932c298d1d930407f5288bed4e4ea5ec659
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-13 04:01:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,10 +31,12 @@ function sayHiToGrandma (string)
   {
   return("YES INDEED!")
   }
-   if (string === "I love you, Grandma")
+   if (string === "I love you, Grandma.")
    {
   return("I love you, too.")
   
+  
+
 }
 
 }
