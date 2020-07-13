a7f3ba3e4093466033d2590ddc5b99784c48639c
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-13 03:59:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ function sayHiToGrandma (string)
 {
   
   if (string.toLowerCase()=== string)
-  {
- return "I can't hear you!")
-}
+  { return ("I can't hear you!")}
  
   if (string.toUpperCase()===string)
   {
